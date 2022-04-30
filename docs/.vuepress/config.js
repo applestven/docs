a5b39616ac78a060f1afd91f356501522f5fa3e9
applestven <applestven@gmail.com>
2022-04-30 19:13:26 +0800

    feat:pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,21 @@
 const moment = require('moment')
 module.exports = {
-  base:'/docs/',
+  // base:'/docs/',
   // SEO 相关配置 
   title:"大青虫", // 网站的标题，它将会被用作所有页面标题的前缀，同时，默认主题下，它将显示在导航栏（navbar）上。
   description:"大青虫的成长日记",
   head:[
-    ['link', { rel: 'icon', href: './assets/img/xiao.png' }],
+    ['link', { rel: 'icon', href: './assets/img/xiao.PNG' }],
     ['meta', { rel: 'author', content: '不爱吃青菜的大青虫' }],
     ['meta', { rel: 'Keywords', content: 'vuepress介绍说明 不爱吃青菜的大青虫' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.PNG' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.PNG' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   // ps:
   // moment插件处理相关
@@ -22,6 +30,16 @@ module.exports = {
           return moment(timestamp).format('llll')
         }
       }
+    ],
+    [
+      '@vuepress/pwa', {
+              serviceWorker: true,
+              updatePopup: {
+                message: "别让鸡零狗碎的琐事耗尽你对生活的向往",
+                buttonText: "刷新"
+              }
+              
+      }
     ]
   ],
   // ps：
@@ -32,7 +50,7 @@ module.exports = {
         // navbar: false,
       
       lastUpdated: '更新时间', // string | boolean
-      logo: './assets/img/xiaosi.png',
+      logo: './assets/img/xiaosi.PNG',
       nav: [
         { text: 'Home', link: '/' },
         { text: 'About', link: '/about.html' },
