0eb1e067773ce17be38f5075933cdc4ed9503606
applestven <applestven@gmail.com.com>
2023-04-27 17:24:53 +0800

    一步到位 ，拜托

---
+++ forCloud.sh
@@ -0,0 +1,12 @@
+echo -e "---------docker Login--------"
+docker login --username=$1  --password=$2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
+echo -e "---------docker Stop--------"
+docker stop docs
+echo -e "---------docker Rm--------"
+docker rm docs
+docker rmi applestven/docs:latest
+echo -e "---------docker Pull--------"
+docker pull applestven/docs:latest 
+echo -e "---------docker Create and Start--------"
+docker run --rm -d -p 9000:80 --name docs applestven/docs:latest
+echo -e "---------deploy Success--------"
